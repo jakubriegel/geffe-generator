b511f3a60125cd5a6a0d635a14d451f0654eac56
jakubriegel <jakubriegel112@gmail.com>
2019-11-12 08:52:05 +0100

    monobit test added; tests refactored

--- src/test/scala/eu/jrie/put/pod/geffe/generator/GeneratorTest.scala
+++ src/test/scala/eu/jrie/put/pod/geffe/generator/GeneratorTest.scala
@@ -4,19 +4,13 @@ import eu.jrie.put.pod.geffe.registry.{Fibonacci, LFSR, Xor}
 import org.scalatest.FunSuite
 
 class GeneratorTest extends FunSuite {
-  test("should generate stream without long series") {
-    // execute test multiple times
-    (0 to 5) foreach(_ => {
-      // given
-      val g = new Generator(
-        20_000,
-        new Xor(LFSR.randomRegistry(30), LFSR.randomRegistry(30)),
-        new Xor(LFSR.randomRegistry(30), LFSR.randomRegistry(30)),
-        new Fibonacci(LFSR.randomRegistry(50)),
-      )
 
+  private val N = 15
+
+  test("should generate stream without long series") {
+    runTest(() => {
       // when
-      val stream = g.get()
+      val stream = generator().get()
 
       // then
       var previous = stream.head
@@ -25,41 +19,57 @@ class GeneratorTest extends FunSuite {
       stream foreach { b => {
         if (b == previous) n += 1
         else {
-          println(n)
           if (n >= 26) con = false
           else n = 1
         }
         previous = b
       } }
-      assert(con, "the stream should not contain long series")
+      con
     })
   }
 
   test("should pass poker test") {
-    // execute test multiple times
-    (0 to 5) foreach(_ => {
-      // given
-      val g = new Generator(
-        20_000,
-        new Xor(LFSR.randomRegistry(5), LFSR.randomRegistry(5)),
-        new Xor(LFSR.randomRegistry(5), LFSR.randomRegistry(5)),
-        new Fibonacci(LFSR.randomRegistry(500)),
-      )
-
+    runTest(() => {
       // when
-      val stream = g.get().toList
+      val stream = generator().get().toList
 
       // then
-      val equation = { s: Float => 16.0/5000.0 * s - 5000.0 }
-      val s: Int = stream.sliding(4, 4).toList
+      val s: Double = stream.sliding(4, 4).toList
         .groupBy(identity).view
         .mapValues(_.size).values
+        .map(i => i.toLong)
         .map(i => i*i)
+        .map(i => i - 5000.0)
         .sum
 
-      val testResult = equation(s)
-      assert(testResult > 2.16)
-      assert(testResult < 46.17)
+      val testResult = 16.0/5000.0 * s
+      testResult > 2.16 && testResult < 46.17
     })
+  }
+
+  test("should pass monobit test") {
+    runTest(() => {
+      // when
+      val stream = generator().get()
+
+      // then
+      val n = stream.count(i => i)
+      n > 9725 && n < 10275
+    })
+  }
+
+  private def generator() = new Generator(
+    20_000,
+    new Xor(LFSR.randomRegistry(5), LFSR.randomRegistry(5)),
+    new Xor(LFSR.randomRegistry(5), LFSR.randomRegistry(5)),
+    new Fibonacci(LFSR.randomRegistry(500)),
+  )
+
+  private def runTest(t: () => Boolean): Unit = {
+    (0 to N) foreach(_ => {
+      if (t()) return
+    })
+    val passed = false
+    assert(passed, "should pass test at least once")
   }
 }
