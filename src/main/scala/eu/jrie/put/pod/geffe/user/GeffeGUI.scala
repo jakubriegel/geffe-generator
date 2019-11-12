a3ed92898e545bf50ec6374ad8dbef0aae9d38e5
jakubriegel <jakubriegel112@gmail.com>
2019-11-12 16:42:03 +0100

    gui basic functionality

--- src/main/scala/eu/jrie/put/pod/geffe/user/GeffeGUI.scala
+++ src/main/scala/eu/jrie/put/pod/geffe/user/GeffeGUI.scala
@@ -1,5 +1,7 @@
 package eu.jrie.put.pod.geffe.user
 
+import eu.jrie.put.pod.geffe.generator.Generator
+import eu.jrie.put.pod.geffe.registry.{Fibonacci, LFSR, Xor}
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
@@ -9,7 +11,21 @@ import scalafx.scene.layout.{HBox, VBox}
 
 class GeffeGUI extends JFXApp {
 
-  def start(): Unit = main(Array())
+  import GeffeGUI.LfsrInput
+
+  private val lengthField = new TextField()
+  private val lfsrInputs = (new LfsrInput(1), new LfsrInput(2), new LfsrInput(3))
+  private val generateButton = new Button("Generate") { onAction = _ => generateAction() }
+  private val resultArea = new TextArea() { editable = false; wrapText = true }
+
+  private def generateAction(): Unit = {
+    resultArea.clear()
+    new Generator(
+      lengthField.getText.toInt,
+      lfsrInputs._1.registry, lfsrInputs._2.registry, lfsrInputs._3.registry
+    ).get()
+      .map(b => if(b) 1 else 0) foreach { b => resultArea.appendText(b.toString) }
+  }
 
   stage = new PrimaryStage {
     resizable = false
@@ -19,49 +35,60 @@ class GeffeGUI extends JFXApp {
         padding = Insets(25)
         children = Seq(
           new HBox(
-            new Label("length"),
-            new TextField()
+            new Label("Length"), lengthField
           ),
-          lfsrInput(1),
-          lfsrInput(2),
-          lfsrInput(3),
-          new Button("Generate"),
-          new TextArea {
-            editable = false
-          }
+          lfsrInputs._1,
+          lfsrInputs._2,
+          lfsrInputs._3,
+          generateButton,
+          resultArea
         )
       }
     }
   }
 
+
+  def start(): Unit = main(Array())
+
+}
+
+object GeffeGUI {
+
   object LFSRType extends Enumeration {
-    type LFSRType = Value
     val XOR, FIBONACCI = Value
   }
 
   import LFSRType._
 
-  private def lfsrInput(lfsrId: Int): VBox = {
-    new VBox {
-      padding = Insets(10, 0, 10, 0)
-      children = Seq(
-        new Label(s"LFSR $lfsrId"),
-        new HBox(
-          new Label("Type"),
-          new ComboBox(Seq(LFSRType.XOR, LFSRType.FIBONACCI)) {
-            value = XOR
-          },
-          new Label("Size"),
-          new TextField()
-        ),
-        new Label("Initial state"),
-        new TextField(),
-        new Label("Coefficients"),
-        new TextField()
-      )
+  class LfsrInput (lfsrId: Int) extends VBox {
+
+    private val typeCombo = new ComboBox(Seq(XOR, FIBONACCI)) { value = XOR }
+    private val sizeField = new TextField()
+    private val initialStateField = new TextField()
+    private val coefficientsField = new TextField()
+
+    padding = Insets(10, 0, 10, 0)
+    children = Seq(
+      new Label(s"LFSR $lfsrId"),
+      new HBox(
+        new Label("Type"), typeCombo,
+        new Label("Size"), sizeField
+      ),
+      new Label("Initial state"), initialStateField,
+      new Label("Coefficients"), coefficientsField
+    )
+
+    def registry: LFSR = {
+      val registrySize = sizeField.getText.toInt
+      typeCombo.getValue match {
+        case XOR => new Xor(initialStateField.getText, coefficientsField.getText)
+        case FIBONACCI => new Fibonacci(initialStateField.getText)
+      }
     }
+
+    private implicit def toRegistry(t: String): List[Boolean] = t.toCharArray
+      .map(b => if (b == '1') true else false)
+      .toList
+
   }
-
-
-
 }
