70d4b13b8e5b9b37aa4084c58e541456c99238d2
jakubriegel <jakubriegel112@gmail.com>
2019-11-17 15:19:19 +0100

    series test added

--- src/test/scala/eu/jrie/put/pod/geffe/generator/GeneratorTest.scala
+++ src/test/scala/eu/jrie/put/pod/geffe/generator/GeneratorTest.scala
@@ -3,11 +3,47 @@ package eu.jrie.put.pod.geffe.generator
 import eu.jrie.put.pod.geffe.registry.{Fibonacci, Xor}
 import org.scalatest.FunSuite
 
+import scala.collection.mutable
+
 class GeneratorTest extends FunSuite {
 
-  private val N = 15
+  private val N = 20_000
 
-  test("should generate stream without long series") {
+  test("should pass series test") {
+    // when
+    val stream = generator().get()
+
+    // then
+    val series = mutable.Map(
+      1 -> 0,
+      2 -> 0,
+      3 -> 0,
+      4 -> 0,
+      5 -> 0,
+      6 -> 0,
+    )
+    var previous = stream.head
+    var n = 0
+    stream foreach { b => {
+      if (b == previous) n += 1
+      else {
+        if (n >= 1 && n <= 6) series(n) = series(n)+1
+        else series(6) = series(6)+1
+        n = 1
+      }
+      previous = b
+    } }
+
+    println(series)
+    assert(series(1) >= 2343 && series(1) <= 2657)
+    assert(series(2) >= 1135 && series(2) <= 1365)
+    assert(series(3) >= 542 && series(3) <= 708)
+    assert(series(4) >= 251 && series(4) <= 373)
+    assert(series(5) >= 111 && series(5) <= 201)
+    assert(series(6) >= 111 && series(6) <= 201)
+  }
+
+  test("should pass long series test") {
     // when
     val stream = generator().get()
 
@@ -35,7 +71,6 @@ class GeneratorTest extends FunSuite {
       .groupBy(identity).view
       .mapValues(_.size).values
       .map(i => { println(s"$i"); i })
-      .map(i => i.toLong)
       .map(i => i*i)
       .map(i => i.toDouble)
       .sum
@@ -58,9 +93,9 @@ class GeneratorTest extends FunSuite {
   }
 
   private def generator() = new Generator(
-    20_000,
+    N,
     new Fibonacci(
-      List(true, false, true, true, true, true, true, true, true, true, true, false, true, true, true, false),
+      List(true, false, true, true, true, false, true, true, true, true, true, false, true, true, true, false),
     ),
     new Xor(
       List(false, true, true, true, false, true, false, true, false, true, false, true, false, true, false, true),
